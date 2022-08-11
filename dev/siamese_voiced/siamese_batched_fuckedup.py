29fb1be6d19f5c8674c9bcd5fdf7f97a19a98251
vrbaj <Jan.Vrba@vscht.cz>
2022-08-11 10:49:13 +0200

    bit o cleanup

--- dev/siamese_voiced/siamese_batched_fuckedup.py
+++ dev/siamese_voiced/siamese_batched_fuckedup.py
@@ -36,242 +36,15 @@ import cv2
 """
 ## Hyperparameters
 """
-
-epochs = 100
+input_size = 280 #272 TOP
 batch_size = 50
+first_run = False
+epochs = 5
 margin = 1  # Margin for constrastive loss.
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 """
 ## Load the MNIST dataset
-"""
-#(x_train_val, y_train_val), (x_test, y_test) = keras.datasets.mnist.load_data()
-
-
-# x_train_val = np.asarray(x_train_val)
-# y_train_val = np.asarray(y_train_val)
-# x_test = np.asarray(x_test)
-# y_test = np.asarray(y_test)
-# # Change the data type to a floating point format
-# x_train_val = x_train_val.astype("float32")
-# x_test = x_test.astype("float32")
-#
-#
-# """
-# ## Define training and validation sets
-# """
-#
-# # Keep 50% of train_val  in validation set
-# x_train, x_val = x_train_val[:30000], x_train_val[30000:]
-# y_train, y_val = y_train_val[:30000], y_train_val[30000:]
-# del x_train_val, y_train_val
-#
-#
-# """
-# ## Create pairs of images
-# We will train the model to differentiate between digits of different classes. For
-# example, digit `0` needs to be differentiated from the rest of the
-# digits (`1` through `9`), digit `1` - from `0` and `2` through `9`, and so on.
-# To carry this out, we will select N random images from class A (for example,
-# for digit `0`) and pair them with N random images from another class B
-# (for example, for digit `1`). Then, we can repeat this process for all classes
-# of digits (until digit `9`). Once we have paired digit `0` with other digits,
-# we can repeat this process for the remaining classes for the rest of the digits
-# (from `1` until `9`).
-# """
-#
-#
-# def make_pairs(x, y):
-#     """Creates a tuple containing image pairs with corresponding label.
-#     Arguments:
-#         x: List containing images, each index in this list corresponds to one image.
-#         y: List containing labels, each label with datatype of `int`.
-#     Returns:
-#         Tuple containing two numpy arrays as (pairs_of_samples, labels),
-#         where pairs_of_samples' shape is (2len(x), 2,n_features_dims) and
-#         labels are a binary array of shape (2len(x)).
-#     """
-#
-#     num_classes = max(y) + 1
-#     digit_indices = [np.where(y == i)[0] for i in range(num_classes)]
-#     print("digit_indices", digit_indices)
-#     pairs = []
-#     labels = []
-#
-#     for idx1 in range(len(x)):
-#         # add a matching example
-#         x1 = x[idx1]
-#         label1 = y[idx1]
-#         idx2 = random.choice(digit_indices[label1])
-#         x2 = x[idx2]
-#
-#         pairs += [[x1, x2]]
-#         labels += [1]
-#
-#         # add a non-matching example
-#         label2 = random.randint(0, num_classes - 1)
-#         while label2 == label1:
-#             label2 = random.randint(0, num_classes - 1)
-#
-#         idx2 = random.choice(digit_indices[label2])
-#         x2 = x[idx2]
-#
-#         pairs += [[x1, x2]]
-#         labels += [0]
-#
-#     return np.array(pairs), np.array(labels).astype("float32")
-
-# pickled_pairs_path = Path("../../data/voiced_pairs_train.pickled")
-# with open(pickled_pairs_path, "rb") as f:
-#     # pairs_train = np.asarray(pickle.load(f))
-#     # labels_train = np.asarray(pickle.load(f), dtype=np.float32)
-#     ds = Dataset.from_dict(pickle.load(f))
-# train_dataset = ds.to_tf_dataset(columns=["inputs"], label_cols=["labels"], batch_size=100, shuffle=True)
-#
-# pickled_pairs_path = Path("../../data/voiced_pairs_validation.pickled")
-# with open(pickled_pairs_path, "rb") as f:
-#     # pairs_val = np.asarray(pickle.load(f))
-#     # labels_val = np.asarray(pickle.load(f), dtype=np.float32)
-#     ds = Dataset.from_dict(pickle.load(f))
-# validation_dataset = ds.to_tf_dataset(columns=["inputs"], label_cols=["labels"], batch_size=10, shuffle=True)
-#
-# pickled_pairs_path = Path("../../data/voiced_pairs_test.pickled")
-# with open(pickled_pairs_path, "rb") as f:
-#     # pairs_test = np.asarray(pickle.load(f))
-#     # labels_test = np.asarray(pickle.load(f), dtype=np.float32)
-#     ds = Dataset.from_dict(pickle.load(f))
-# test_dataset = ds.to_tf_dataset(columns=["inputs"], label_cols=["labels"], batch_size=10, shuffle=True)
-# input_size = 224
-# # make train pairs
-# pairs_train, labels_train = make_pairs(x_train, y_train)
-# print(pairs_train[:3], labels_train[:3])
-# # make validation pairs
-# pairs_val, labels_val = make_pairs(x_val, y_val)
-#
-# # make test pairs
-# pairs_test, labels_test = make_pairs(x_test, y_test)
-
-"""
-We get:
-**pairs_train.shape = (60000, 2, 28, 28)**
-- We have 60,000 pairs
-- Each pair contains 2 images
-- Each image has shape `(28, 28)`
-"""
-
-"""
-Split the training pairs
-"""
-#
-# x_train_1 = pairs_train[:, 0]  # x_train_1.shape is (60000, 28, 28)
-# x_train_2 = pairs_train[:, 1]
-
-"""
-Split the validation pairs
-"""
-#
-# x_val_1 = pairs_val[:, 0]  # x_val_1.shape = (60000, 28, 28)
-# x_val_2 = pairs_val[:, 1]
-
-"""
-Split the test pairs
-"""
-#
-# x_test_1 = pairs_test[:20, 0]  # x_test_1.shape = (20000, 28, 28)
-# x_test_2 = pairs_test[:20, 1]
-#
-
-"""
-## Visualize pairs and their labels
-"""
-
-
-def visualize(pairs, labels, to_show=6, num_col=3, predictions=None, test=False):
-    """Creates a plot of pairs and labels, and prediction if it's test dataset.
-    Arguments:
-        pairs: Numpy Array, of pairs to visualize, having shape
-               (Number of pairs, 2, 28, 28).
-        to_show: Int, number of examples to visualize (default is 6)
-                `to_show` must be an integral multiple of `num_col`.
-                 Otherwise it will be trimmed if it is greater than num_col,
-                 and incremented if if it is less then num_col.
-        num_col: Int, number of images in one row - (default is 3)
-                 For test and train respectively, it should not exceed 3 and 7.
-        predictions: Numpy Array of predictions with shape (to_show, 1) -
-                     (default is None)
-                     Must be passed when test=True.
-        test: Boolean telling whether the dataset being visualized is
-              train dataset or test dataset - (default False).
-    Returns:
-        None.
-    """
-
-    # Define num_row
-    # If to_show % num_col != 0
-    #    trim to_show,
-    #       to trim to_show limit num_row to the point where
-    #       to_show % num_col == 0
-    #
-    # If to_show//num_col == 0
-    #    then it means num_col is greater then to_show
-    #    increment to_show
-    #       to increment to_show set num_row to 1
-    num_row = to_show // num_col if to_show // num_col != 0 else 1
-
-    # `to_show` must be an integral multiple of `num_col`
-    #  we found num_row and we have num_col
-    #  to increment or decrement to_show
-    #  to make it integral multiple of `num_col`
-    #  simply set it equal to num_row * num_col
-    to_show = num_row * num_col
-
-    # Plot the images
-    fig, axes = plt.subplots(num_row, num_col, figsize=(5, 5))
-    for i in range(to_show):
-
-        # If the number of rows is 1, the axes array is one-dimensional
-        if num_row == 1:
-            ax = axes[i % num_col]
-        else:
-            ax = axes[i // num_col, i % num_col]
-
-        ax.imshow(tf.concat([pairs[i][0], pairs[i][1]], axis=1), cmap="gray")
-        ax.set_axis_off()
-        if test:
-            ax.set_title("True: {} | Pred: {:.5f}".format(labels[i], predictions[i][0]))
-        else:
-            ax.set_title("Label: {}".format(labels[i]))
-    if test:
-        plt.tight_layout(rect=(0, 0, 1.9, 1.9), w_pad=0.0)
-    else:
-        plt.tight_layout(rect=(0, 0, 1.5, 1.5))
-    plt.show()
-
-
-"""
-Inspect training pairs
-"""
-
-#visualize(pairs_train[:-1], labels_train[:-1], to_show=4, num_col=4)
-
-"""
-Inspect validation pairs
-"""
-
-#visualize(pairs_val[:-1], labels_val[:-1], to_show=4, num_col=4)
-
-"""
-Inspect test pairs
-"""
-
-#visualize(pairs_test[:-1], labels_test[:-1], to_show=4, num_col=4)
-
-"""
-## Define the model
-There are be two input layers, each leading to its own network, which
-produces embeddings. A `Lambda` layer then merges them using an
-[Euclidean distance](https://en.wikipedia.org/wiki/Euclidean_distance) and the
-merged output is fed to the final network.
 """
 
 # Provided two tensors t1 and t2
@@ -293,8 +66,6 @@ def euclidean_distance(vects):
 def euclidean_distance_output_shape(shapes):
     shape1, shape2 = shapes
     return (shape1[0], 1)
-
-input_size = 280 #224
 
 
 input = layers.Input((input_size, input_size, 3))
@@ -322,32 +93,19 @@ x = layers.MaxPooling2D(pool_size=(2, 2), padding="same", strides=2)(x)
 # x = layers.Dropout(0.1)(x)
 
 x = layers.Flatten()(x)
-# x = layers.Dense(256, activation="tanh")(x)
-# x = tf.keras.layers.BatchNormalization()(x)
-# x = layers.Dense(10, activation="tanh")(x)
 x = layers.Dense(4096, activation="relu")(x)
 x = layers.Dense(4096, activation="relu")(x)
 x = layers.Dense(4096, activation="relu")(x)
-#x = layers.Dense(1024, activation="relu")(x)
-#x = layers.Dense(1024, activation="relu")(x)
 embedding_network = keras.Model(input, x)
 
 
 input_1 = layers.Input((input_size, input_size, 3))
 input_2 = layers.Input((input_size, input_size, 3))
 
-# As mentioned above, Siamese Network share weights between
-# tower networks (sister networks). To allow this, we will use
-# same embedding network for both tower networks.
 tower_1 = embedding_network(input_1)
 tower_2 = embedding_network(input_2)
-#
-# sum_square = reduce_sum(square(x - y), axis=1, keepdims=True)
-# return sqrt(maximum(sum_square, epsilon()))
+
 merge_layer = layers.Lambda(lambda x: sqrt(maximum(reduce_sum(square(x[0]-x[1]), axis=1, keepdims=True), epsilon())))([tower_1, tower_2])
-# merge_layer = layers.Lambda(euclidean_distance, name="euclidean_distance", output_shape=euclidean_distance_output_shape)([tower_1, tower_2])
-# merge_layer = layers.Lambda(lambda x: keras.backend.sum(keras.backend.abs(x), axis=-1, keepdims=True),
-#         name='euclidean')([tower_1, tower_2])
 
 normal_layer = tf.keras.layers.BatchNormalization()(merge_layer)
 output_layer = layers.Dense(1, activation="sigmoid")(normal_layer)
@@ -410,8 +168,6 @@ def contrastive_loss(y_true, y_pred):
 """
 ## Compile the model with the contrastive loss
 """
-
-# siamese.compile(loss=loss(margin=margin), optimizer="RMSprop", metrics=["accuracy"])
 siamese.compile(loss=contrastive_loss, optimizer="RMSprop", metrics=["accuracy"])
 siamese.summary()
 siamese.save("./siamese_tf")
@@ -423,8 +179,7 @@ siamese = tf.keras.models.load_model("./siamese_tf", custom_objects=({
 """
 ## Train the model
 """
-first_run = False
-epochs = 5
+
 
 with open(Path("../../data/splited_voiced/val/voiced_pairs_paths_00001.pickled"), "rb") as f:
     data = pickle.load(f)
@@ -438,10 +193,10 @@ with open(Path("../../data/splited_voiced/val/voiced_pairs_paths_00001.pickled")
         pairs_val.append([image1, image2])
     labels_val = np.asarray(data["labels"], dtype=np.float32)
     pairs_val = np.asarray(pairs_val)
-    print("shape pairs: ",pairs_val.shape)
+
 x_val_1 = pairs_val[:, 0]  # x_val_1.shape = (60000, 28, 28)
-print("x_val_1 shape: ",x_val_1.shape)
 x_val_2 = pairs_val[:, 1]
+
 for train_dataset_path in Path("../../data/splited_voiced/train").glob("voiced_pairs_path*.pickled"):
     print(f"Training dataset: {train_dataset_path}")
     if first_run:
@@ -466,12 +221,7 @@ for train_dataset_path in Path("../../data/splited_voiced/train").glob("voiced_p
     x_train_1 = pairs_train[:, 0]  # x_train_1.shape is (60000, 28, 28)
     x_train_2 = pairs_train[:, 1]
 
-    """
-    Split the validation pairs
-    """
-    #
-
-    siamese.fit(
+    history = siamese.fit(
         [x_train_1, x_train_2], labels_train,
         validation_data=([x_val_1, x_val_2], labels_val),
         batch_size=batch_size,
@@ -479,19 +229,6 @@ for train_dataset_path in Path("../../data/splited_voiced/train").glob("voiced_p
     )
     siamese.save("./siamese_tf")
 
-# history = siamese.fit(
-#     [x_train_1, x_train_2],
-#     labels_train,
-#     validation_data=([x_val_1, x_val_2], labels_val),
-#     batch_size=batch_size,
-#     epochs=epochs,
-# )
-# history = siamese.fit(
-#     train_dataset,
-#     validation_data=validation_dataset,
-#     batch_size=batch_size,
-#     epochs=epochs,
-# )
 
 """
 ## Visualize results
@@ -518,11 +255,11 @@ def plt_metric(history, metric, title, has_valid=True):
     plt.show()
 
 
-# # Plot the accuracy
-# plt_metric(history=history.history, metric="accuracy", title="Model accuracy")
-#
-# # Plot the constrastive loss
-# plt_metric(history=history.history, metric="loss", title="Constrastive Loss")
+# Plot the accuracy
+plt_metric(history=history.history, metric="accuracy", title="Model accuracy")
+
+# Plot the constrastive loss
+plt_metric(history=history.history, metric="loss", title="Constrastive Loss")
 
 """
 ## Evaluate the model
