f74dac8612bcec84305a5f2eb7dfe9f614c85d18
vrbaj <Jan.Vrba@vscht.cz>
2023-01-12 16:27:37 +0100

    dataset breakthru

--- dev/data_pipeline.py
+++ dev/data_pipeline.py
@@ -2,6 +2,11 @@ import os
 import sys
 import csv
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from keras.utils import image_dataset_from_directory
 from scipy.io import wavfile
 from scipy import signal
 import numpy as np
@@ -48,7 +53,8 @@ def data_pipeline(wav_chunks: int, octaves: list, fft_len: int, fft_overlap: int
             f"ch{wav_chunks}_res{spectrogram_resolution[0]}x{spectrogram_resolution[1]}", subdir_name)
 
     if not DESTINATION_SPECTROGRAM_PATH.is_dir():
-        DESTINATION_SPECTROGRAM_PATH.mkdir(parents=True, exist_ok=True)
+        DESTINATION_SPECTROGRAM_PATH.joinpath("nonhealthy").mkdir(parents=True, exist_ok=True)
+        DESTINATION_SPECTROGRAM_PATH.joinpath("healthy").mkdir(parents=True, exist_ok=True)
         print(f"created folder {DESTINATION_SPECTROGRAM_PATH}")
         with open(CSV_PATH.joinpath("datasets_info.csv"), "a", encoding="UTF8", newline="") as csv_file:
             writer = csv.writer(csv_file, quoting=csv.QUOTE_NONE, escapechar='')
@@ -66,13 +72,45 @@ def data_pipeline(wav_chunks: int, octaves: list, fft_len: int, fft_overlap: int
             ax.set_axis_off()
             fig.add_axes(ax)
             ax.pcolormesh(times, frequencies, spectrogram, cmap="gray")
+            destination_path = DESTINATION_SPECTROGRAM_PATH.joinpath("healthy")
+            if "nonhealthy" in str(sound_file):
+                destination_path = DESTINATION_SPECTROGRAM_PATH.joinpath("nonhealthy")
 
-            plt.savefig(DESTINATION_SPECTROGRAM_PATH.joinpath(sound_file.stem + ".png"), format="png",
+            plt.savefig(destination_path.joinpath(sound_file.stem + ".png"), format="png",
                         bbox_inches='tight', pad_inches=0, dpi=300)
             plt.close("all")
 
     return DESTINATION_SPECTROGRAM_PATH
 
 
-print(data_pipeline(4, [4,6], 256, 128 // 2, (224, 224)))
+path = data_pipeline(4, [4,6], 256, 128 // 2, (224, 224))
 
+train = tf.keras.preprocessing.image_dataset_from_directory(
+  path,
+  seed=123,
+  validation_split=0.2,
+  subset="training",
+  image_size=(224, 224),
+  batch_size=128)
+
+val = tf.keras.preprocessing.image_dataset_from_directory(
+  path,
+  seed=123,
+  validation_split=0.2,
+  subset="validation",
+  image_size=(224, 224),
+  batch_size=128)
+model = tf.keras.Sequential()
+
+# Build the model.
+model.add(layers.Flatten(input_shape=(224, 224,3)))
+model.add(layers.Dense(4096, activation='relu', input_shape=(224, 224,3)))
+model.add(layers.Dense(4096, activation='relu'))
+model.add(layers.Dense(1, activation="sigmoid"))
+
+# Display the model summary.
+model.summary()
+focal_loss = tf.keras.losses.BinaryCrossentropy()
+optimizer_cnn = tf.keras.optimizers.Adam(learning_rate=0.00001)
+model.compile(loss=focal_loss, optimizer=optimizer_cnn, metrics=["accuracy"])
+model.fit(train, validation_data=val, epochs=30)
