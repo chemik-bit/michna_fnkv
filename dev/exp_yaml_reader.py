615cb665594145f690b39fcb1737fdc332399dbe
vrbaj <Jan.Vrba@vscht.cz>
2023-04-25 14:01:14 +0200

    exp_yaml_reader.py added

---
+++ dev/exp_yaml_reader.py
@@ -0,0 +1,10 @@
+import yaml
+
+# read yaml file
+with open("cnn_pipeline_config.yaml") as file:
+    yaml_data = yaml.safe_load(file)
+
+    print(yaml_data)
+
+for key in yaml_data.keys():
+    print(f"SECTION -------------------------------------- {key}: {yaml_data[key]}")
