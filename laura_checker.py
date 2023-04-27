aaed6c0f0ae7560007e9424a14c2395001226178
vrbaj <Jan.Vrba@vscht.cz>
2023-04-27 13:42:31 +0200

    laura_checker.py added, pipeline results log tweaked (lowest FP+ FN)

---
+++ laura_checker.py
@@ -0,0 +1,24 @@
+laura_results = {"f1": 72.99,
+                 "precision": 80.65,
+                 "recall": 66.67,
+                 "accuracy": 64.42,
+                 "specificity": 58.62,
+                 "auc": 0.626}
+
+TP = 125
+TN = 21
+FP = 36
+FN = 26
+
+our_results = {"f1": 100 * 2 * TP / (2 * TP + TN + FP + FN),
+               "precision":100 * TP / (TP + FP),
+               "recall": 100 *TP / (TP + FN),
+               "accuracy": 100 *(TP + TN) / (TP + TN + FP + FN),
+               "specificity": 100 *TN / (TN + FP),
+               "auc": 0.628}
+
+for key in our_results:
+    if our_results[key] > laura_results[key]:
+        print(f"{key} - pass {our_results[key]} vs {laura_results[key]}")
+    else:
+        print(f"{key} - failed  {our_results[key]} vs {laura_results[key]}")
