3e957f77ef70c0a740288837f1bd9875ca2671c5
steinbaj <steinbach.jakub@gmail.com>
2023-07-14 14:22:28 +0200

    different combinations of filter size, max pool size and stride, and dropout for the 2conc2x4 model

---
+++ src/cnn/models/2conv_2x4_best/cnn_2conv2x4_filters_80_dropout_80_maxpool1x2_strides2x2_gap.py
@@ -0,0 +1,17 @@
+from tensorflow.keras import layers
+import tensorflow as tf
+
+
+def create_model(input_size):
+    x = tf.keras.Sequential()
+    x.add(layers.Conv2D(80, (2, 4), activation="relu", padding="same", input_shape=(input_size[0], input_size[1], 1)))
+    x.add(layers.BatchNormalization())
+    x.add(layers.MaxPooling2D(pool_size=(1, 2), strides=(2, 2)))
+    x.add(layers.Conv2D(80, (2, 4), activation="relu", padding="same", input_shape=(input_size[0], input_size[1], 1)))
+    x.add(layers.BatchNormalization())
+    x.add(layers.MaxPooling2D(pool_size=(1, 2), strides=(2, 2)))
+    x.add(layers.Dropout(80))
+    x.add(layers.GlobalAveragePooling2D())
+    x.add(layers.Dense(1, activation="sigmoid"))
+    x.compile(loss=tf.losses.binary_crossentropy, optimizer="adam", metrics=["val_accuracy"])
+    return x
